5c7b589faf9448ad95c5a434c9794b64934fc3fa
emc <0xemc@protonmail.com>
2023-08-03 08:58:24 +1000

    Write to DB

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import concurrent.futures
 import pandas as pd
 from tree_detect import predict_tile_img
 from shapely.geometry import Polygon
-from constants import MT_WELLINGTON_BOUNDING_BOX, API_KEY
-from utils.geo import tiles_in_polygon, point_in_tile_to_latlon
+from constants import MT_WELLINGTON_BOUNDING_BOX, API_KEY, POSTGRES_URL
+from utils.geo import tiles_in_polygon
 from utils.mapbox import batch_fetch_tile_image
+from sqlalchemy import create_engine
+from geoalchemy2 import Geometry
 
+# ------ Fetch Tile Images ------ #
 # Build our Shapely polygon from our BOUNDING_BOX
 polygon = Polygon(MT_WELLINGTON_BOUNDING_BOX)
 
@@ -19,6 +22,8 @@ batch_fetch_tile_image(tiles, 18, API_KEY)
 # Get a list of all files in the 'images' directory
 image_files = os.listdir("images")
 
+
+# -------  Find the Trees  -------- #
 # Create a ThreadPoolExecutor
 with concurrent.futures.ThreadPoolExecutor() as executor:
     # Use list comprehension to create a list of futures
@@ -31,5 +36,16 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 
     merged_results = pd.concat(results, ignore_index=True)
 
-
 print(merged_results)
+
+#  ----- Write to DB ------
+engine = create_engine(POSTGRES_URL)
+
+# Write DataFrame to PostGIS table "trees"
+merged_results.to_sql(
+    "trees",
+    engine,
+    if_exists="append",
+    index=False,
+    dtype={"geom": Geometry("POINT", srid=4326)},
+)
--- src/tree_detect.py
+++ src/tree_detect.py
@@ -1,48 +1,64 @@
 import os
+import h3
+import re
 import matplotlib.pyplot as plt
 import pandas as pd
 from deepforest import main
 from utils.geo import point_in_tile_to_latlon
+from geoalchemy2 import WKTElement
+
 
 def predict(image_path, plot=False):
     model = main.deepforest()
-    model.use_release()
+    model.use_release(check_release=False)
     return model.predict_image(path=image_path, return_plot=plot)
 
+
 def show(data):
-    plt.imshow(data[:,:,::-1])
-    plt.show() 
+    plt.imshow(data[:, :, ::-1])
+    plt.show()
+
 
 def process_row(row):
-    lat, lon = point_in_tile_to_latlon(238316, 165684, row['x_norm'], row['y_norm'], 18)
+    lat, lon = point_in_tile_to_latlon(238316, 165684, row["x_norm"], row["y_norm"], 18)
     return pd.Series([lat, lon])
+
 
 # This function relies on the naming format of tile_x_y_zoom.jpg in order to get the tile details
 def predict_tile_img(image_file):
-    if image_file.endswith('.jpg'):
-        file_path = os.path.join('images', image_file)
-        
-        import re
-        
+    if image_file.endswith(".jpg"):
+        file_path = os.path.join("images", image_file)
+
         # Extract the tile x and y from the image_file path
-        match = re.search(r'tile_(\d+)_(\d+)_\d+.jpg', image_file)
+        match = re.search(r"tile_(\d+)_(\d+)_\d+.jpg", image_file)
 
         if not match:
-            raise ValueError("File format did not match as expected. Expected format: 'tile_x_y_zoom.jpg'")
+            raise ValueError(
+                "File format did not match as expected. Expected format: 'tile_x_y_zoom.jpg'"
+            )
 
         tile_x, tile_y = map(int, match.groups())
 
         # Count the trees!
         result = predict(file_path)
-        
+
         # Calculate the center points
-        result['x_center'] = (result['xmin'] + result['xmax']) / 2 
-        result['y_center'] = (result['ymin'] + result['ymax']) / 2 
-        result['x_norm'] = result['x_center'] / 512
-        result['y_norm'] = result['y_center'] / 512
+        result["x_center"] = (result["xmin"] + result["xmax"]) / 2
+        result["y_center"] = (result["ymin"] + result["ymax"]) / 2
+        result["x_norm"] = result["x_center"] / 512
+        result["y_norm"] = result["y_center"] / 512
 
-      # Define process_row as a lambda function
-        process_row = lambda row: pd.Series(point_in_tile_to_latlon(tile_x, tile_y, row['x_norm'], row['y_norm'], 18))
+        # Define process_row as a lambda function
+        add_lat_lng = lambda row: pd.Series(
+            point_in_tile_to_latlon(tile_x, tile_y, row["x_norm"], row["y_norm"], 18)
+        )
 
-        result[['lat', 'lon']] = result.apply(process_row, axis=1)
-        return result[['lat','lon']]
+        # Convert lat and lon to a Point geometry (this assumes lon and lat are in degrees)
+        add_point_geometry = lambda row: WKTElement(
+            f"POINT({row.lon} {row.lat})", srid=4326
+        )
+
+        result[["lat", "lon"]] = result.apply(add_lat_lng, axis=1)
+        result["geom"] = result.apply(add_point_geometry, axis=1)
+
+        return result[["geom", "image_path", "score", "lat", "lon"]]
