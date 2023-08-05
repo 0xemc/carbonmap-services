a30f4d280b6848154a88cbb992e86a8bf2cfd614
emc <0xemc@protonmail.com>
2023-08-06 05:25:15 +1000

    Disable vercel deployment

--- api/fetch.py
+++ api/fetch.py
@@ -11,15 +11,15 @@ bounding_box = BoundingBox(name="NewNorfolk", shape=NEW_NORFOLK_BOUNDING_BOX)
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            query = urlparse(self.path).query
-            params = parse_qs(query)
+            # query = urlparse(self.path).query
+            # params = parse_qs(query)
 
-            res = int(params.get("res", [18])[0])
-            limit = int(params.get("limit", [100])[0])
+            # res = int(params.get("res", [18])[0])
+            # limit = int(params.get("limit", [100])[0])
 
-            fetch(bounding_box, res, limit)
+            # fetch(bounding_box, res, limit)
             self.send_response(200)
-            self.wfile.write(f"Success".encode("utf-8"))
+            self.wfile.write(f"Disabled".encode("utf-8"))
         except Exception as e:
             self.send_response(500)
             self.wfile.write(str(e).encode("utf-8"))
